01fda6e533e1d8f1a86cbdda04d5ed28191ef5bc
luciano minni <lucianominni@gmail.com>
2026-04-30 16:16:25 +0200

    refactor: remove unused economy section from Squadra component

--- src/components/squadra/Squadra.tsx
+++ src/components/squadra/Squadra.tsx
@@ -98,23 +98,6 @@ export default function Squadra({ idSquadra }: SquadraProps) {
                     Terzo: {datiAlbo.terzo}
                   </Typography>
                 </Grid>
-                <Grid item xs={6} md={12} sx={{ mt: 2, mb: 2 }}>
-                  <Typography color="primary" variant="h6">
-                    <u>Economia</u>
-                  </Typography>
-                  <Typography color="primary" variant="body1">
-                    Quota annua: {datiSquadra.importoAnnuale} €
-                  </Typography>
-                  <Typography color="primary" variant="body1">
-                    Multe: {datiSquadra.importoMulte} €
-                  </Typography>
-                  <Typography color="primary" variant="body1">
-                    Mercato gennaio: {datiSquadra.importoMercato} €
-                  </Typography>
-                  <Typography color="primary" variant="body1">
-                    Fantamilioni rimanenti: {datiSquadra.fantamilioni}
-                  </Typography>
-                </Grid>
               </Grid>
             )
           })()}
