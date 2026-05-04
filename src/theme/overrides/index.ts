bc520ca22a225ade09fbd70eb8577c2fcc54b0b8
luciano minni <lucianominni@gmail.com>
2026-05-04 14:52:39 +0200

    feat: add CssBaseline component for consistent styling across the application

--- src/theme/overrides/index.ts
+++ src/theme/overrides/index.ts
@@ -4,12 +4,17 @@ import type { Theme, Components } from '@mui/material/styles'
 import merge from 'lodash/merge'
 
 // project import
+import Autocomplete from './Autocomplete'
 import Badge from './Badge'
 import Button from './Button'
+import Card from './Card'
 import CardContent from './CardContent'
+import CardHeader from './CardHeader'
 import Checkbox from './Checkbox'
 import Chip from './Chip'
+import CssBaseline from './CssBaseline'
 import DataGrid from './DataGrid'
+import FormControlLabel from './FormControlLabel'
 import IconButton from './IconButton'
 import InputLabel from './InputLabel'
 import LinearProgress from './LinearProgress'
@@ -18,13 +23,9 @@ import ListItemIcon from './ListItemIcon'
 import OutlinedInput from './OutlinedInput'
 import Tab from './Tab'
 import TableCell from './TableCell'
+import TableHead from './TableHead'
 import Tabs from './Tabs'
 import Typography from './Typography'
-import CardHeader from './CardHeader'
-import Autocomplete from './Autocomplete'
-import FormControlLabel from './FormControlLabel'
-import TableHead from './TableHead'
-import Card from './Card'
 
 // ==============================|| OVERRIDES - MAIN ||============================== //
 
@@ -38,6 +39,7 @@ export default function ComponentsOverrides(theme: Theme) {
     CardContent(),
     Checkbox(theme),
     Chip(theme),
+    CssBaseline(theme),
     DataGrid(theme),
     FormControlLabel(theme),
     IconButton(theme),
